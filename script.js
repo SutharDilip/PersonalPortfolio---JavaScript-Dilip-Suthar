668cddad29bcf987206bb3605495f949fb0f4a1a
Dilip <dilip@123>
2021-07-13 20:19:21 +0530

    Portfoliio

---
+++ script.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass('sticky');
+        } else {
+            $('.navbar').removeClass('sticky');
+        }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass('show');
+        } else {
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+
+    // scroll function
+
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    // Toggle menu / navbar Script
+
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing Animation script
+
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Frontend Developer", "BackEnd Developer", "Designer", "freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Frontend Developer", "BackEnd Developer", "Designer", "freelancer"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        loop: true
+
+    });
+    var typed = new Typed("footer", {
+        strings: ["Thanks for visitng", "Created by Dilip @ 2021 All Rights Reserved"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+
+
+    // owl Script
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    })
+});
